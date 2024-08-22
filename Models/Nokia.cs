1b1af573e2f3a59aab69287be032446d1ef27d70
Duda Duarte <90913069+dudaduarte07@users.noreply.github.com>
2024-08-22 10:21:07 -0400

    Add files via upload

---
+++ Models/Nokia.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace DesafioCelular.Models
+{
+    public class Nokia : Smartphone
+    {
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine(nomeApp + " foi instalado corretamente!");
+        }
+    }
+}
